f41c923a4ff073bdc94f324ece930235634e0052
rdalf2 <rdalf2@student.monash.edu>
2018-04-16 21:34:24 +1000

    Start up

--- src/main/java/Corrency/Converter/App.java
+++ src/main/java/Corrency/Converter/App.java
@@ -1,13 +1,18 @@
 package Corrency.Converter;
+import javax.money.convert.*;
+import javax.money.spi.*;
+
 
 /**
  * Hello world!
  *
  */
 public class App 
-{
+{	
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
     }
+   
+    
 }
