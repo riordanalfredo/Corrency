86871414d33c6e6dd1a12a4ef474a480e0d9ccde
rdalf2 <rdalf2@student.monash.edu>
2018-04-16 21:21:10 +1000

    first init

---
+++ src/main/java/Corrency/Converter/App.java
@@ -0,0 +1,13 @@
+package Corrency.Converter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
